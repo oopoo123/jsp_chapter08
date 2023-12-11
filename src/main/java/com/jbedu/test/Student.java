33159e92557c89932a707a03f5ae1be9b5e11712
oopoo123 <oopoo123@naver.com>
2023-12-11 10:43:01 +0900

    수정

--- src/main/java/com/jbedu/test/Student.java
+++ src/main/java/com/jbedu/test/Student.java
@@ -1,50 +1,45 @@
 package com.jbedu.test;
 
-import javax.management.loading.PrivateClassLoader;
-
 public class Student {
 	
-	private String Name; // 학생이름
-	private int Age;//나이
-	private int Grade;//학년
+	private String name;//학생이름
+	private int age;//나이
+	private int grade;//학년
+	
 	
 	public Student() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public String getName() {
-		return Name;
-	}
-
 	public Student(String name, int age, int grade) {
 		super();
-		Name = name;
-		Age = age;
-		Grade = grade;
+		this.name = name;
+		this.age = age;
+		this.grade = grade;
 	}
-
+	
+	public String getName() {
+		return name;
+	}
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
 	}
-
 	public int getAge() {
-		return Age;
+		return age;
 	}
-
 	public void setAge(int age) {
-		Age = age;
+		this.age = age;
 	}
-
 	public int getGrade() {
-		return Grade;
+		return grade;
 	}
-
 	public void setGrade(int grade) {
-		Grade = grade;
+		this.grade = grade;
 	}
 	
 	
-		
+	
+	
 
 }
